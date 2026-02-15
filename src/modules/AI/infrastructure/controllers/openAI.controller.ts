8d6fa75b7e805e3068aeab6612f5e48041ab39a0
MrPloman <polplanaayet@gmail.com>
2026-02-15 11:00:21 -0600

    first endpoint defined

--- src/modules/AI/infrastructure/controllers/openAI.controller.ts
+++ src/modules/AI/infrastructure/controllers/openAI.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import type { Response } from 'express';
 
 @Controller('ai')
-export class OpenAIController {}
+export class OpenAIController {
+  @Post('generateRecipe')
+  async generateRecipe(@Body() body: any, @Res() res: Response) {
+    await res.status(300).send('hola');
+  }
+  @Post('replanRecipe')
+  public async replanRecipe() {
+    return 'entra again';
+  }
+}
