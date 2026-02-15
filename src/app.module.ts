c6e674cbd0f5fd918f070fd983aae4662fd219ae
MrPloman <polplanaayet@gmail.com>
2026-02-14 22:32:49 -0600

    rmeoved controlers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AiModule } from './modules/AI/ai.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { RecipesController } from './modules/recipes/recipes.controller';
-import { RecipesModule } from './modules/recipes/recipes.module';
-import { RecipesService } from './modules/recipes/recipes.service';
 import { ListsModule } from './modules/lists/lists.module';
+import { RecipesModule } from './modules/recipes/recipes.module';
 
 @Module({
   imports: [AuthModule, AiModule, RecipesModule, ListsModule],
-  controllers: [AppController, RecipesController],
-  providers: [AppService, RecipesService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
